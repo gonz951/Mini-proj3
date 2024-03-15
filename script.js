a6c9eb4fc1f482ac646afbb62f9fe948073d39ac
Fel <felgonzalez951@gmail.com>
2024-03-14 21:34:58 -0600

    a lot

---
+++ script.js
@@ -0,0 +1,15 @@
+//window.prompt("Put in 'r' for rock, 'p' for paper, and 's' for scissors.")
+window.alert("hi");
+window.confirm("Do you want to play Rock, Paper, Scissors?")
+let person = prompt("Put in 'r' for rock, 'p' for paper, and 's' for scissors.");
+if (person !=null) {
+    close 
+}
+//window.confirm("Do you want to play Rock, Paper, Scissors?");
+//if (confirm) {
+//    window.prompt("Put in 'r' for rock, 'p' for paper, and 's' for scissors.")
+//} else {
+//    txt = "aw"
+//}
+
+//window.prompt("Put in 'r' for rock, 'p' for paper, and 's' for scissors.")
